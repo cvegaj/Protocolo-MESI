8d5130e364c992c6af4bf95c1394a446da24239d
lennon <lennon@nume>
2016-11-20 21:15:54 -0600

    act

--- main_org.cpp
+++ main_org.cpp
@@ -9,7 +9,6 @@
 using namespace std;
 
 struct block{
-  char State = 'I'; //MESI
   bool bValidBit = false;
   long lBlockTag = 0;
 };
@@ -61,18 +60,14 @@ bool cache::readAddress(long lAddr){
   splitAddress();
   for(int i=0; i<iAssociativity; i++){ //para buscar la cantidad de bloques
     if (vBlocks[lIndex][i].bValidBit){
-      if (vBlocks[lIndex][i].lBlockTag == lTag){//Si si esta el bloque
-          return true
+      if (vBlocks[lIndex][i].lBlockTag == lTag){
+        return true;
       }
     }
   }
-  iRand = rand() % iAssociativity;//Si no esta el bloque
+  iRand = rand() % iAssociativity;
   vBlocks[lIndex][iRand].bValidBit = true;
-  vBlocks[lIndex][iRand].lBlockTag = lTag;//Jala el bloque
-
-  if (vBlocks[lIndex][i].State == 'I'){// Si en el read
-      
-
+  vBlocks[lIndex][iRand].lBlockTag = lTag;
 
   return false;
 }
@@ -81,45 +76,63 @@ bool cache::readAddress(long lAddr){
 //se reciben los parametros del programa
 int main(int arcg, char* argv[]){
 
+  //long long llParameter[3];
+  //llParameter[0]=atoll(argv[1]);
+  //llParameter[1]=atoll(argv[2]);
+  //llParameter[2]=atoll(argv[3]);
+
   ifstream datos("aligned.trace");
 	long lAddr;
-        char lAct;
 	bool bHit;
 
 	long long llTotalCPU0 = 0;
 	long long llHitsCPU0 = 0;
 	long long llMissesCPU0 = 0;
 	double dMissRateCPU0;
+
+        long long llTotalCPU1 = 0;
+	long long llHitsCPU1 = 0;
+	long long llMissesCPU1 = 0;
+	double dMissRateCPU1;
   string line;
-  string line_copy;
 
   //cantidad de accesos totales, hits y misses
-  cache CacheCPU0(1,8000,16);
-  cache CacheCPU1(1,8000,16);
-  cache CacheShared(1,64000,16);
+  cache CacheL1(1,8000,16);
+  cache CacheL2(1,64000,16);
   int CPU;
   CPU = 0;
 
   while ( getline (datos,line) ) {
 
     getline(datos,line);
-    line_copy = line;
     line.erase(line.begin()+8,line.end());
-    istringstream(line) >> std::hex >> lAddr; //Obtiene direccion
+    istringstream(line) >> std::hex >> lAddr;
 
-    lAct = line_copy.at(line_copy.size()-1); //Obtiene si es lectura (L) o escritura (S)
-
-    if(CPU == 0 ) {//CPU0
-        if(lAct == 'S'){//Escritura
-  	    bHit = CacheCPU0.readAddress(lAddr);
-        }else{
-            //
-        }
+    if(CPU == 0 ) {
+  	bHit = CacheL1.readAddress(lAddr);
+    	if(bHit){
+      	    llHitsCPU0++;
+    	}
+    	llTotalCPU0++;
 	CPU = 1;
-    }else{//CPU1
-	bHit = CacheCPU1.readAddress(lAddr);
+    }else{
+	bHit = CacheL2.readAddress(lAddr);
+    	if(bHit){
+      	    llHitsCPU1++;
+    	}
+    	llTotalCPU1++;
         CPU = 0;
     }
   }
-  //cout << "Hits CPU0: " << llHitsCPU0 << endl;
+  cout << "Hits CPU0: " << llHitsCPU0 << endl;
+  cout << "Total CPU0: " << llTotalCPU0 << endl;
+  llMissesCPU0 = llTotalCPU0 - llHitsCPU0;
+  dMissRateCPU0 = (double(llMissesCPU0)/double(llTotalCPU0))*100;
+  cout << "Miss Rate CPU0: " << dMissRateCPU0 << endl;
+
+  cout << "Hits CPU1: " << llHitsCPU1 << endl;
+  cout << "Total CPU1: " << llTotalCPU1 << endl;
+  llMissesCPU1 = llTotalCPU1 - llHitsCPU1;
+  dMissRateCPU1 = (double(llMissesCPU1)/double(llTotalCPU1))*100;
+  cout << "Miss Rate CPU1: " << dMissRateCPU1 << endl;
 }
